abc5918e76af4a0402ff1e1a1e25eb5edc8d01fc
Jose <joseguerreroroman@yahoo.com.mx>
2018-03-27 15:06:26 -0600

    Ejercicios de JavaScript
    Ejercicios de Javascript

---
+++ JSejercicios/js/main.js
@@ -0,0 +1,102 @@
+
+function load(selectedLink, element) {
+
+    console.log(selectedLink);
+    console.log(selectedLink.getAttribute("data-content-url"));
+
+    var url = selectedLink.getAttribute("data-content-url");
+    
+    req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+
+  
+    element.innerHTML = req.responseText;
+    let cambio = document.getElementsByClassName('active');
+    
+    // 'cambio' es un Objeto, con solo un elemento, con una propiedad "class",que con este setter podemos finalmente remover la clase 'active'
+   
+    cambio[0].classList.remove('active');
+
+
+    // $("li").removeClass("active");  ESTE FUE EL TEXTO DE LA CLASE
+
+
+    selectedLink.classList.add('active');
+
+}
+
+
+function saveUser() {
+
+    var userObject = {};
+
+    userObject.userName = document.getElementById('user-name').value;    
+    userObject.userMail = document.getElementById('user-mail').value;
+    userObject.pass = document.getElementById('user-pass').value;
+    userObject.confPass = document.getElementById('confirm-user-pass').value;
+
+    console.log(userObject);
+    
+
+    var sameValues = (userObject.pass === userObject.confPass);
+    
+    let caso = 0;
+    let reg =/[@]/;
+    let datos = userObject.userMail;
+    console.log(typeof(datos));
+    console.log(datos);
+    
+    
+    let result = reg.test(userObject.userMail);
+    console.log(result);
+
+    if (userObject.userName === ""){
+        caso = 1;
+    } else if (userObject.userMail === "" || !result) {
+        caso = 2;
+    } else if (userObject.pass === "" || userObject.confPass === ""){
+        caso = 3;
+    } else if (userObject.pass !== userObject.confPass) {
+        caso = 4;
+    } else {
+        successfulRegister();
+        document.getElementById("registroForm").reset();
+        return;
+    }
+
+    alerta(caso);
+
+}
+
+
+function alerta(caso){
+    let cajaAlerta = document.getElementById('alert-box');
+
+   if (caso === 1){
+        cajaAlerta.innerHTML = "<hr><p class=\"text-center\">No capturaste tu nombre</p>";
+    } else if (caso === 2){
+        cajaAlerta.innerHTML = "<hr><p class=\"text-center\">No capturaste un correo válido</p>";
+    } else if (caso === 3){
+        cajaAlerta.innerHTML = "<hr><p class=\"text-center\">No capturaste una constraseña!</p>";
+    } else if (caso === 4) {
+        cajaAlerta.innerHTML = "<hr><p class=\"text-center\">Tu contraseña no coincide</p>";
+    } else {
+        return;
+    }
+   
+    cajaAlerta.classList.remove('hidden');
+
+    setTimeout(function(){cajaAlerta.classList.add('hidden')}, 3000);
+
+    return;
+
+}
+
+
+function successfulRegister() {
+    alert("usuario registrado");
+
+}
+
+
